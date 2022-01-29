5715e298728fc83742c734462a44a67f2ea2031f
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-01-28 21:22:04 -0600

    added to pom.xml file for jpa ORM and tested hello world api endpoint in postman and worked success!

--- anime/src/main/java/com/example/anime/controller/AnimeController.java
+++ anime/src/main/java/com/example/anime/controller/AnimeController.java
@@ -1,4 +1,15 @@
 package com.example.anime.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
 public class AnimeController {
+
+    @GetMapping(path = "/hello-world/")
+    public String getHelloWorld(){
+        return "hello world";
+    }
 }
