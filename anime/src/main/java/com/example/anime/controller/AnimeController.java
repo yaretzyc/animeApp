335ed3d0efc8a46afeb713ab8bd725cd6e8e2593
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-01-30 12:16:34 -0600

    api endpoints for the summary-oneToOne with anime-

--- anime/src/main/java/com/example/anime/controller/AnimeController.java
+++ anime/src/main/java/com/example/anime/controller/AnimeController.java
@@ -1,6 +1,7 @@
 package com.example.anime.controller;
 
 import com.example.anime.model.Anime;
+import com.example.anime.model.Summary;
 import com.example.anime.service.AnimeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -53,7 +54,31 @@ public class AnimeController {
         return animeService.deleteAnime(animeId);
     }
 
+///////////////////////////////SUMMARY\\\\\\\\\\\\\\\\\\\
 
+    //CREATE SUMMARY
+    @PostMapping("/animes/{animeId}/summary")
+    public Summary createAnimeSummary(@PathVariable (value = "animeId")Long animeId){
+        return animeService.createAnimeSummary(animeId);
+    }
+
+    //GET SUMMARY
+    @GetMapping("/animes/{animeId}/summary")
+    public Summary getAnimeSummary(@PathVariable(value = "animeId")Long animeId){
+        return animeService.getAnimeSummary(animeId);
+    }
+
+    //UPDATE SUMMARY
+    @PutMapping("/animes/{animeId}/summary")
+    public Summary updateAnimeSummary(@PathVariable(value = "animeId")Long animeId){
+        return animeService.updateAnimeSummary(animeId);
+    }
+
+    //DELETE SUMMARY
+    @DeleteMapping("/animes/{animeId}/summary")
+    public Summary deleteAnimeSummary(@PathVariable(value = "animeId")Long animeId){
+        return animeService.deleteAnimeSummary(animeId);
+    }
 
 
 
